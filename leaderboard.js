f967b3944ceea68c281e70a28283a60f8f3c9595
Praveen Kumar <praveenscience@gmail.com>
2021-04-06 23:09:51 +0100

    Implemented ranks correctly.

--- leaderboard.js
+++ leaderboard.js
@@ -1,13 +1,17 @@
 (function () {
   const table_data = document.querySelector(".table tbody");
   $.getJSON("/UsersTable.json", function (results) {
+    scores = results
+      .map(r => r.Score)
+      .filter((value, index, self) => self.indexOf(value) === index)
+      .sort((a, b) => +b - +a);
     table_data.innerHTML = "";
     htmldata = "";
     results.forEach((participant, index) => {
       htmldata += `
         <tr class="hover-color">
           <td class="table-data-id">${index + 1}</td>
-          <td class="table-data-rank">${index + 1}</td>
+          <td class="table-data-rank">${scores.indexOf(participant.Score) + 1}</td>
           <td class="table-username">
             <a href="https://github.com/${participant.Username}" target="_blank">${participant.Username}</a>
           </td>
