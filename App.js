5638f78526a6df7daaf009d181b506b263f20f19
Praveen Kumar <praveenscience@gmail.com>
2021-04-06 14:48:23 +0100

    Added all the scores for labels.

--- App.js
+++ App.js
@@ -3,3 +3,9 @@ const Data = require("./Data.json");
 console.log("Hello!");
 console.log("Total Projects: " + Object.keys(Data).length);
 console.log(Data[Object.keys(Data)[0]][0].labels);
+const score = {
+  level0: 5,
+  level1: 10,
+  level2: 25,
+  level3: 45
+};
