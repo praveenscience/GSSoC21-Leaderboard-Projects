db7233f5e313a779d7ad065543b990fa49353365
Praveen Kumar <praveenscience@gmail.com>
2021-04-06 19:01:35 +0100

    Handled null values.
    * If the desired label is not available, the PR gets a zero score.
    * Stopped writing the JSON.
    * Created an empty array of null scoring users (not just PRs).
    * All those users' PRs are added sorted by users.
    * Wrote contents to nullValues.txt.

--- App.js
+++ App.js
@@ -13,10 +13,9 @@ const score = {
 const need = Object.keys(score);
 const calcScore = labels => {
   labels = labels.map(l => l.name.toLowerCase()).sort();
-  return [
-    labels.filter(value => need.includes(value))[0],
-    score[labels.filter(value => need.includes(value))[0]]
-  ];
+  labels = labels.filter(value => need.includes(value))[0];
+  if (!labels) return ["No Label", 0];
+  return [labels, score[labels]];
 };
 const AllPRs = Object.values(Data).flat();
 console.log("Total Eligible PRs: " + AllPRs.length);
@@ -38,22 +37,30 @@ const UsersPRs = AllPRs.reduce((acc, pr) => {
 console.log("Total Users Contributed: " + Object.keys(UsersPRs).length);
 console.log("--- Files ---");
 console.log("Writing UsersPRs file...");
-fs.writeFileSync("UsersPRs.json", JSON.stringify(UsersPRs));
+const nullValues = [];
+if (false) fs.writeFileSync("UsersPRs.json", JSON.stringify(UsersPRs));
 console.log(
   "Successfully written " +
     JSON.stringify(UsersPRs).length +
     " bytes UsersPRs file."
 );
-const UsersTable = Object.keys(UsersPRs).map(Username => ({
-  Username,
-  PRCount: UsersPRs[Username].length,
-  Score: UsersPRs[Username].reduce((a, b) => a + +b.Score, 0)
-}));
+const UsersTable = Object.keys(UsersPRs).map(Username => {
+  if (!UsersPRs[Username].reduce((a, b) => a + +b.Score, 0)) {
+    nullValues.push(UsersPRs[Username].map(u => ({ Username, ...u })));
+  }
+  return {
+    Username,
+    PRCount: UsersPRs[Username].length,
+    Score: UsersPRs[Username].reduce((a, b) => a + +b.Score, 0)
+  };
+});
+fs.writeFileSync("nullValues.txt", JSON.stringify(nullValues.flat()));
 console.log("Writing UsersTable file...");
-fs.writeFileSync(
-  "UsersTable.json",
-  JSON.stringify(UsersTable.sort((a, b) => b.Score - a.Score))
-);
+if (false)
+  fs.writeFileSync(
+    "UsersTable.json",
+    JSON.stringify(UsersTable.sort((a, b) => b.Score - a.Score))
+  );
 console.log(
   "Successfully written " +
     JSON.stringify(UsersTable.sort((a, b) => b.Score - a.Score)).length +
