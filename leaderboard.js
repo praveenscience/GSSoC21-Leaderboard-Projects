211901d6e7e9f15181547f20475cd0a3505b387a
Praveen Kumar <praveenscience@gmail.com>
2021-04-06 17:20:28 +0100

    Implemented the leaderboard renderer.

---
+++ leaderboard.js
@@ -0,0 +1,23 @@
+(function () {
+  const table_data = document.querySelector(".table tbody");
+  $.getJSON("/UsersTable.json", function (results) {
+    table_data.innerHTML = "";
+    results.forEach((participant, index) => {
+      table_data.innerHTML += `
+        <tr class="hover-color">
+          <td class="table-data-id">${index}</td>
+          <td class="table-data-rank">${index}</td>
+          <td class="table-username">
+            <a href="https://github.com/${participant.Username}" target="_blank">${participant.Username}</a>
+          </td>
+          <td class="table-data-rank">${participant.PRCount}</td>
+          <td class="table-score">${participant.Score}</td>
+        </tr>
+      `;
+    });
+  });
+})();
+
+tippy("[data-tippy-content]", {
+  theme: "translucent"
+});
