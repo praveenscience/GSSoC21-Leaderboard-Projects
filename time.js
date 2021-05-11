4c4109dbc0dadc0bfdef2a0a47545c62cd9efe9d
Praveen Kumar <praveenscience@gmail.com>
2021-05-11 04:56:41 +0100

    Added AutoTime Script.

---
+++ time.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+console.clear();
+const Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+const getOrdinalNum = n =>
+  n +
+  (n > 0
+    ? ["th", "st", "nd", "rd"][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10]
+    : "");
+const timeInIndia = new Date(new Date().getTime() + 270 * 60000);
+const CurTime = `${getOrdinalNum(timeInIndia.getDate())} ${
+  Months[timeInIndia.getMonth()]
+} ${timeInIndia.getHours() % 12}:${timeInIndia.getMinutes()} ${
+  timeInIndia.getHours() >= 12 ? "pm" : "am"
+} IST`;
+console.log(CurTime);
+fs.writeFileSync("./UpdatedTime.txt", CurTime);
