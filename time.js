0777d28e42a3f6a1215263b4552cbf27c85002e3
Praveen Kumar <praveenscience@gmail.com>
2021-06-09 21:03:56 +0100

    Testing cut time.

--- time.js
+++ time.js
@@ -18,13 +18,17 @@ const getOrdinalNum = n =>
   (n > 0
     ? ["th", "st", "nd", "rd"][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10]
     : "");
-const timeInIndia = new Date(new Date().getTime() + 270 * 60000);
+const timeInIndia = new Date(
+  Math.floor(new Date().getTime() + (270 * 60000) / (15 * 60000)) * 15 * 60000
+);
 const CurTime = `${getOrdinalNum(timeInIndia.getDate())} ${
   Months[timeInIndia.getMonth()]
 } ${
   timeInIndia.getHours() > 12
     ? timeInIndia.getHours() - 12
-    : timeInIndia.getHours() === 0 ? 12 : timeInIndia.getHours()
+    : timeInIndia.getHours() === 0
+    ? 12
+    : timeInIndia.getHours()
 }:${("0" + timeInIndia.getMinutes()).slice(-2)} ${
   timeInIndia.getHours() >= 12 ? "pm" : "am"
 } IST`;
